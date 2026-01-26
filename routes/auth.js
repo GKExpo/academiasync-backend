a3fe48cef0fba02f7e9d187553329777c4bf0ff3
GKExpo <theshardulkamble@gmail.com>
2026-01-26 17:42:11 +0530

    Fix case-sensitive model import for Render (User.js)

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import Attendance from '../models/Attendance.js';
+import AttendanceRequest from '../models/AttendanceRequest.js';
+import LeaveRequest from '../models/LeaveRequest.js';
+
 
 const router = express.Router();
 
