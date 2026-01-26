e5c1391ce4a188748a839943683ba5e2a41051af
GKExpo <theshardulkamble@gmail.com>
2026-01-26 22:59:03 +0530

    Fixed server.js

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ const PORT = process.env.PORT || 5000;
    MIDDLEWARE
 ========================= */
 
-app.use(cors({
-   origin: [
-      'http://localhost:3000',
-      'http://localhost',
-      'https://localhost',
-      'capacitor://localhost',
-      'https://academiasync-backend.onrender.com'
-   ],
-   credentials: true
-}));
-
+app.use(
+   cors({
+      origin: [
+         'http://localhost:3000',
+         'capacitor://localhost',
+         'http://localhost',
+         'https://academiasync-backend.onrender.com'
+      ],
+      credentials: true
+   })
+);
 
 
 app.use(express.json());
