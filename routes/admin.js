9ecf31b55f57009c239fc1ba312a5cad2e761b2f
GKExpo <theshardulkamble@gmail.com>
2026-01-31 23:14:43 +0530

    Fix missing allowRoles import in admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ import AttendanceRequest from '../models/AttendanceRequest.js';
 import LeaveRequest from '../models/LeaveRequest.js';
 import { protect } from '../middleware/authMiddleware.js';
 import Attendance from '../models/Attendance.js';
+import { allowRoles } from '../middleware/roleMiddleware.js';
 
 
 const router = express.Router();
